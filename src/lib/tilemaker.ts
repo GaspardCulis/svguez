72e9bac6d45802e587fd83ef7cf5d5e57756be8b
GaspardCulis <gasdev.lp@gmail.com>
2024-04-25 14:45:11 +0200

    feature(tilemaker): Implement createTiles function

--- src/lib/tilemaker.ts
+++ src/lib/tilemaker.ts
@@ -13,7 +13,6 @@ export function createTile(
   let min_size = tile_size / 100;
 
   const bbox = new DOMRect(x * tile_size, y * tile_size, tile_size, tile_size);
-  console.log(bbox);
 
   const svg_tile = filterSVG(svg, (element) => {
     if (element instanceof SVGGraphicsElement) {
@@ -38,4 +37,26 @@ export function createTile(
   );
 
   return svg_tile;
+}
+
+export function createTiles(
+  svg: SVGSVGElement,
+  max_zoom: number
+): Map<string, SVGSVGElement> {
+  const out = new Map<string, SVGSVGElement>();
+  const initial_width = Math.ceil(svg.width.baseVal.value / BASE_TILE_SIZE);
+  const initial_height = Math.ceil(svg.height.baseVal.value / BASE_TILE_SIZE);
+
+  for (let z = 0; z <= max_zoom; z++) {
+    for (let y = 0; y < initial_height * Math.pow(2, z); y++) {
+      for (let x = 0; x < initial_width * Math.pow(2, z); x++) {
+        let svg_tile = createTile(svg, z, x, y);
+        let key = `${z}/${x}/${y}.svg`;
+        out.set(key, svg_tile);
+        console.log(`Created tile ${key}`);
+      }
+    }
+  }
+
+  return out;
 }
