724d37dc9b5a43ae0a32fcb87bee6fb93c7ad814
GaspardCulis <gasdev.lp@gmail.com>
2024-04-25 14:58:29 +0200

    feat(utils): Implement saveBlob function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -50,3 +50,16 @@ export function filterSVG(
 
   return out;
 }
+
+export function saveBlob(blob: Blob, filename: string) {
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+}
