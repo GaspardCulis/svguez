80c8d5ea4f3897ff7a83dbacfc3f897a8290c2bb
GaspardCulis <gasdev.lp@gmail.com>
2024-04-25 11:47:14 +0200

    refactor(tilemake): Moved everything to more generic utils functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,48 @@ export async function loadSVG(name: string): Promise<SVGSVGElement> {
   return new DOMParser().parseFromString(svg, "image/svg+xml")
     .documentElement as unknown as SVGSVGElement;
 }
+
+export function intersects(rect_a: DOMRect, rect_b: DOMRect): boolean {
+  return !(
+    rect_a.x + rect_a.width < rect_b.x ||
+    rect_a.x > rect_b.x + rect_b.width ||
+    rect_a.y + rect_a.height < rect_b.y ||
+    rect_a.y > rect_b.y + rect_b.height
+  );
+}
+
+function filterSVGworker(
+  elements: NodeListOf<ChildNode>,
+  new_element: SVGElement,
+  predicate: (element: ChildNode) => boolean
+) {
+  for (let element of elements) {
+    if (predicate(element)) {
+      let new_node = element.cloneNode(false) as SVGElement;
+      new_element.appendChild(new_node);
+
+      if (element.hasChildNodes()) {
+        filterSVGworker(
+          element.childNodes as NodeListOf<SVGGraphicsElement>,
+          new_node as SVGElement,
+          predicate
+        );
+      }
+
+      if (element instanceof SVGTextElement) {
+        new_node.innerHTML = element.innerHTML;
+      }
+    }
+  }
+}
+
+export function filterSVG(
+  element: SVGSVGElement & SVGGraphicsElement,
+  predicate: (element: ChildNode) => boolean
+): SVGSVGElement {
+  let out = element.cloneNode(false) as SVGSVGElement;
+
+  filterSVGworker(element.childNodes, out, predicate);
+
+  return out;
+}
