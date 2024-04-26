d385574070dbb815c07810bbb08d56af8430227c
GaspardCulis <gasdev.lp@gmail.com>
2024-04-26 13:48:00 +0200

    feat(Stats): Added tile count stat

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -9,6 +9,8 @@ export const statsStore = {
   currentZoom: atom(0),
   progress: atom(0.0),
   outputSize: atom(0),
+  tileCount: atom(0),
+  finalTilesCount: atom(0),
 };
 
 export const inputSVG = atom<SVGSVGElement | undefined>(undefined);
