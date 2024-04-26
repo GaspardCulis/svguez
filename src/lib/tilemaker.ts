f6d7d54e4fc784eb9fac3f8de2314d1324304e44
GaspardCulis <gasdev.lp@gmail.com>
2024-04-26 14:33:08 +0200

    feat(tilemaker.ts): Update statsStore

--- src/lib/tilemaker.ts
+++ src/lib/tilemaker.ts
@@ -1,4 +1,4 @@
-import { settingsStore } from "./store";
+import { settingsStore, statsStore } from "./store";
 import { filterSVG, intersects } from "./utils";
 
 export function createTile(
@@ -77,6 +77,11 @@ function createTilesChunked(
     }
   }
 
+  statsStore.tileCount.set(tiles.size);
+  statsStore.progress.set(
+    (100 * tiles.size) / statsStore.finalTilesCount.get()
+  );
+
   requestAnimationFrame(() =>
     createTilesChunked(svg, max_zoom, tiles, resolve, x, y, z)
   );
@@ -87,6 +92,16 @@ export function createTiles(
   max_zoom: number
 ): Promise<Map<string, SVGSVGElement>> {
   const tiles = new Map<string, SVGSVGElement>();
+  const base_tile_size = settingsStore.tileSize.get();
+  const initial_width = Math.ceil(svg.width.baseVal.value / base_tile_size);
+  const initial_height = Math.ceil(svg.height.baseVal.value / base_tile_size);
+
+  let totalTiles = 0;
+  for (let z = 0; z <= max_zoom; z++) {
+    totalTiles += initial_width * initial_height * Math.pow(2, 2 * z);
+  }
+  statsStore.finalTilesCount.set(totalTiles);
+
   return new Promise<Map<string, SVGSVGElement>>((resolve) => {
     createTilesChunked(svg, max_zoom, tiles, resolve, 0, 0, 0);
   });
