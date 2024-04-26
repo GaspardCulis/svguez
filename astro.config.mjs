e86dc67ba9efba9813a514a27eba049f2223ae58
GaspardCulis <gasdev.lp@gmail.com>
2024-04-26 14:56:55 +0200

    feat: etup GH pages deployment GH action

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://GaspardCulis.github.io",
+  base: "svguez",
   integrations: [tailwind()],
 });
