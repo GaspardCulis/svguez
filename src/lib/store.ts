0b0f7e996c2bf17f6041ab95b905f0640d3d27d7
GaspardCulis <gasdev.lp@gmail.com>
2024-04-26 13:06:20 +0200

    feat(lib): Create store.ts

---
+++ src/lib/store.ts
@@ -0,0 +1,12 @@
+import { atom } from "nanostores";
+
+export const settingsStore = {
+  maxZoomLevel: atom(0),
+  tileSize: atom(0),
+};
+
+export const statsStore = {
+  currentZoom: atom(0),
+  progress: atom(0.0),
+  outputSize: atom(0.0),
+};
