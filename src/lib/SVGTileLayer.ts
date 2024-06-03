b19be4f0f59cb9523a31b9f69a9f342ad35372b0
GaspardCulis <gasdev.lp@gmail.com>
2024-06-03 15:05:01 +0200

    feat(SVGTileLayer): Update stroke width on zoom

--- src/lib/SVGTileLayer.ts
+++ src/lib/SVGTileLayer.ts
@@ -1,10 +1,35 @@
 import L from "leaflet";
 
+class SVGTile {
+  private paths = new Map<SVGPathElement, number>();
+
+  constructor(readonly svg: SVGElement) {
+    this.svg
+      .querySelectorAll("path")
+      .forEach((e) =>
+        this.paths.set(e, parseInt(e.getAttribute("stroke-width") || "1"))
+      );
+  }
+
+  updateStrokes(zoom: number) {
+    this.paths.forEach((initialStrokeWidth, path) => {
+      path.setAttribute(
+        "stroke-width",
+        `${initialStrokeWidth / Math.pow(zoom, 2)}` // Formule à revoir
+      );
+    });
+  }
+}
+
 export default class SVGTileLayer extends L.GridLayer {
+  private tiles = new Map<L.Coords, SVGTile>();
+  private current_zoom: number = 0;
+
   constructor() {
     super({
       tileSize: 1024,
     });
+    this.on("tileunload", this._onTileRemove);
   }
 
   protected createTile(coords: L.Coords, done: L.DoneCallback): HTMLElement {
@@ -17,6 +42,9 @@ export default class SVGTileLayer extends L.GridLayer {
           svg.style.width = "100%";
           svg.style.height = "100%";
 
+          const svgTile = new SVGTile(svg);
+          svgTile.updateStrokes(this.current_zoom);
+          this.tiles.set(coords, svgTile);
           tile.appendChild(svg);
         }
         done(undefined, tile);
@@ -25,6 +53,25 @@ export default class SVGTileLayer extends L.GridLayer {
 
     // Return temporary tile
     return tile;
+  }
+
+  addTo(map: L.Map): this {
+    this.current_zoom = map.getZoom();
+    map.on("zoomend", () => {
+      this.current_zoom = map.getZoom();
+      // Mettre à jour les attributs `stroke-width` des éléments `<path>` uniquement si dans la plage de zoom visible
+      const t0 = performance.now();
+      this.tiles.forEach((tile) => {
+        tile.updateStrokes(this.current_zoom);
+      });
+      console.log(`Updated paths stroke-width in ${performance.now() - t0}ms`);
+    });
+
+    return super.addTo(map);
+  }
+
+  private _onTileRemove(event: L.TileEvent) {
+    this.tiles.delete(event.coords);
   }
 
   private async _loadSVG(coords: L.Coords): Promise<HTMLElement | null> {
