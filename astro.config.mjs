50ee6e307f9cf4b49541db10f5df922189bf155f
GaspardCulis <gasdev.lp@gmail.com>
2024-06-06 17:13:51 +0200

    feat(astro.config): Added 'worker' integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import worker from "@astropub/worker"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://GaspardCulis.github.io",
   base: "/svguez",
-  integrations: [tailwind()],
+  integrations: [tailwind(),worker()],
 });
