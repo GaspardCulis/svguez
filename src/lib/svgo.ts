c0d9015ecfad365a6824a054e8e872644044cdb7
GaspardCulis <gasdev.lp@gmail.com>
2024-06-06 17:27:49 +0200

    feat(SVGUpload): Now optimize uploaded SVG with SVGO

---
+++ src/lib/svgo.ts
@@ -0,0 +1,22 @@
+import { type Config, optimize } from "svgo";
+
+const config: Config ={
+    plugins: [
+        {
+            name: 'preset-default',
+            params: {
+                overrides: {
+                    removeViewBox: false,
+                    cleanupIds: false,
+                },
+            },
+        },
+    ],
+};
+
+addEventListener("message", (e) => {
+    const svgText = e.data as string;
+    const output = optimize(svgText, config);
+    
+    postMessage(output.data);
+})
