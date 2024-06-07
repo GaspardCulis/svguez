08729123022746a74236e2f916b595156db0f11f
GaspardCulis <gasdev.lp@gmail.com>
2024-06-07 09:59:43 +0200

    chore(astro.config + package.json): Removed '@astropub/worker' integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import worker from "@astropub/worker"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://GaspardCulis.github.io",
   base: "/svguez",
-  integrations: [tailwind(),worker()],
+  integrations: [tailwind()],
 });
