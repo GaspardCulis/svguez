4c8482994b3e103e406381f0476a59782465fe25
GaspardCulis <gasdev.lp@gmail.com>
2024-06-20 14:10:43 +0200

    feat(svg-tilemaker.py): Implement all SVGuezTilemaker setter and action methods

--- scripts/svg-tilemaker.py
+++ scripts/svg-tilemaker.py
@@ -1,18 +1,71 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
 import os
+from time import sleep
 from argparse import ArgumentParser
 
 class SVGuezTilemaker():
-    def __init__(self, page_url: str, backend: str = "chrome") -> None:
+    def __init__(self, backend: str = "chrome") -> None:
         self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) if backend == "chrome" else webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+    def load(self, page_url: str):
         self.driver.get(page_url)
+        return self
+
+    def generate(self):
+        wait = WebDriverWait(self.driver, 60)
+        start_button = wait.until(EC.element_to_be_clickable((By.ID, "start-button")))
+        start_button.click()
+        return self
+
+    def upload_svg(self, path: str):
+        file_upload = self.driver.find_element(By.ID, "file-upload")
+        file_upload.send_keys(path)
+        return self
+
+    def set_max_zoom(self, value: int):
+        zoom_level_input = self.driver.find_element(By.ID, "max-zoom-input")
+        for _ in range(32): # Set to minimum, 32 is arbitrary
+            zoom_level_input.send_keys(Keys.LEFT)
+        for _ in range(value):
+            zoom_level_input.send_keys(Keys.RIGHT)
+
+        return self
+    
+    def set_tile_size(self, value: int):
+        tile_size_select = Select(self.driver.find_element(By.ID, "tile-size-select"))
+        try:
+            tile_size_select.select_by_value(str(value))
+        except NoSuchElementException:
+            raise IndexError("The specified tile-size argument isn't available, try exponents of two (256, 512, 1024, 2048...)")
+
+        return self
         
+    def set_remove_small(self, value: bool):
+        self._set_checkbox("remove-small-input", value)
+        return self
+
+    def set_keep_on_final(self, value: bool):
+        self._set_checkbox("remove-small-input", value)
+        return self
+
+    def _set_checkbox(self, id: str, checked: bool):
+        checkbox = self.driver.find_element(By.ID, id)
+        is_checked = checkbox.is_selected()
+
+        if (checked and not is_checked) or (is_checked and not checked):
+            checkbox.click()
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -39,5 +92,13 @@ if __name__ == "__main__":
     remove_small: bool = args.remove_small
     keep_on_final: bool = args.keep_on_final
 
-    tilemaker = SVGuezTilemaker(page_url, backend)
+    tilemaker = SVGuezTilemaker(backend) \
+        .load(page_url) \
+        .upload_svg(svg_path) \
+        .set_max_zoom(max_zoom_level) \
+        .set_tile_size(tile_size) \
+        .set_remove_small(remove_small) \
+        .set_keep_on_final(keep_on_final) \
+        .generate()
     
+    sleep(3600)
