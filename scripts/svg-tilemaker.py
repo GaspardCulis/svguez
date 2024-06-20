7172a8df370fd6c060f970650bffa29233e332d5
GaspardCulis <gasdev.lp@gmail.com>
2024-06-20 11:53:21 +0200

    feat(svg-tilemaker.py): Added new "backend" argument

--- scripts/svg-tilemaker.py
+++ scripts/svg-tilemaker.py
@@ -1,14 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 import os
 from argparse import ArgumentParser
 
 class SVGuezTilemaker():
-    def __init__(self, page_url: str) -> None:
-        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    def __init__(self, page_url: str, backend: str = "chrome") -> None:
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) if backend == "chrome" else webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
         self.driver.get(page_url)
         
 
@@ -18,6 +20,7 @@ if __name__ == "__main__":
     parser.add_argument("-z", "--max-zoom-level", help="The max zoom level to generate", type=int, default=5)
     parser.add_argument("-s", "--tile-size", help="The desired tile size", type=int, default=1024)
     parser.add_argument("-u", "--page-url", help="The SVGuez tilemaker page URL, defaults to the SVGUEZ_TILEMAKER_URL env variable value", type=str)
+    parser.add_argument("-b", "--backend", help="The web driver backend", choices=["chrome", "firefox"], default="chrome")
     parser.add_argument("--remove-small", help="Remove small enough elements from low-zoom levels", action="store_true")
     parser.add_argument("--keep-on-final", help="Keep all small elements when generating the final zoom level", action="store_true")
     args = parser.parse_args()
@@ -31,8 +34,10 @@ if __name__ == "__main__":
     page_url: str | None = args.page_url if args.page_url else os.environ.get("SVGUEZ_TILEMAKER_URL")
     assert page_url, "Please specify the --page-url parameter or define the SVGUEZ_TILEMAKER_URL to the SVGuez tilemaker page URL."
 
+    backend: str = args.backend
+
     remove_small: bool = args.remove_small
     keep_on_final: bool = args.keep_on_final
 
-    tilemaker = SVGuezTilemaker(page_url)
+    tilemaker = SVGuezTilemaker(page_url, backend)
     
