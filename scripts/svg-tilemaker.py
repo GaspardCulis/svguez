822ecc41fd67912e8e70e32ca6915e4b29add0b7
GaspardCulis <gasdev.lp@gmail.com>
2024-06-20 11:23:48 +0200

    feat(svg-tilemaker.py): Defined args

--- scripts/svg-tilemaker.py
+++ scripts/svg-tilemaker.py
@@ -2,4 +2,11 @@ import selenium
 from argparse import ArgumentParser
 
 if __name__ == "__main__":
-    print("Hello world")
+    parser = ArgumentParser()
+    parser.add_argument("svg_path", help="The path of the SVG image", type=str)
+    parser.add_argument("-z", "--max-zoom-level", help="The max zoom level to generate", type=int)
+    parser.add_argument("-s", "--tile-size", help="The desired tile size", type=int)
+    parser.add_argument("--remove-small", help="Remove small enough elements from low-zoom levels", action="store_true")
+    parser.add_argument("--keep-on-final", help="Keep all small elements when generating the final zoom level", action="store_true")
+    args = parser.parse_args()
+    print(args)
