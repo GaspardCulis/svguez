6468d2dc4fc66d8edb032da6751f22bde48d24ed
GaspardCulis <gaspard@gasdev.fr>
2024-07-03 15:33:59 +0200

    refactor(tilemaker): TileMaker is now a class with TileStorage interface

--- src/lib/tilemaker.ts
+++ src/lib/tilemaker.ts
@@ -1,104 +1,125 @@
-import { settingsStore, statsStore } from "./store";
+import { statsStore } from "./store";
 import { filterSVG, intersects } from "./utils";
 
-export function createTile(
-  svg: SVGSVGElement,
-  z: number,
-  x: number,
-  y: number,
-  max_zoom: number,
-): SVGSVGElement {
-  const base_tile_size = settingsStore.tileSize.get();
-  let tile_size = base_tile_size / Math.pow(2, z);
-
-  const checkSize =
-    settingsStore.removeSmall.get() &&
-    !(z === max_zoom && settingsStore.keepFinal.get());
-  const min_size = tile_size / 300;
-
-  const bbox = new DOMRect(x * tile_size, y * tile_size, tile_size, tile_size);
-
-  const svg_tile = filterSVG(svg, (element) => {
-    if (element instanceof SVGGraphicsElement) {
-      // Start with unprecise but fast `getBBox` method
-      let element_bbox = element.getBBox();
-
-      if (!intersects(bbox, element_bbox)) {
-        return false;
-      } else if (checkSize) {
-        // Compute bbox with more precise but expensive `getBoundingClientRect` method for width and height checking
-        element_bbox = element.getBoundingClientRect();
-        if (element_bbox.height < min_size && element_bbox.width < min_size) {
-          return false;
-        }
-      }
-    }
-
-    return true;
-  });
-
-  // Update viewport
-  svg_tile.setAttribute(
-    "viewBox",
-    `${bbox.x} ${bbox.y} ${bbox.width} ${bbox.height}`,
-  );
-
-  return svg_tile;
+export interface TileStorage {
+  size: number;
+  set(coords: string, tile: SVGSVGElement): void;
+  [Symbol.iterator](): Iterator<[string, SVGSVGElement]>;
 }
 
-function createTilesChunked(
-  svg: SVGSVGElement,
-  max_zoom: number,
-  tiles: Map<string, SVGSVGElement>,
-  resolve: (value: Map<string, SVGSVGElement>) => void,
-  x: number,
-  y: number,
-  z: number,
-) {
-  const base_tile_size = settingsStore.tileSize.get();
-  const initial_width = Math.ceil(svg.width.baseVal.value / base_tile_size);
-  const initial_height = Math.ceil(svg.height.baseVal.value / base_tile_size);
+type TilemakerParams = {
+  tileSize: number;
+  maxZoomLevel: number;
+  removeSmall: boolean;
+  keepFinal: boolean;
+};
 
-  for (let i = 0; i < 20; i++) {
-    // Process 20 tiles at a time
-    let svg_tile = createTile(svg, z, x, y, max_zoom);
-    let key = `${z}/${x}/${y}.svg`;
-    tiles.set(key, svg_tile);
+export default class Tilemaker {
+  constructor(
+    readonly svg: SVGSVGElement,
+    readonly storage: TileStorage,
+    readonly params: TilemakerParams,
+  ) {}
 
-    x++;
-    if (x >= initial_width * Math.pow(2, z)) {
-      x = 0;
-      y++;
-      if (y >= initial_height * Math.pow(2, z)) {
-        z++;
-        if (z > max_zoom) {
-          resolve(tiles);
-          return;
+  createTile(
+    svg: SVGSVGElement,
+    z: number,
+    x: number,
+    y: number,
+    max_zoom: number,
+  ): SVGSVGElement {
+    let tile_size = this.params.tileSize / Math.pow(2, z);
+
+    const checkSize =
+      this.params.removeSmall && !(z === max_zoom && this.params.keepFinal);
+    const min_size = tile_size / 300;
+
+    const bbox = new DOMRect(
+      x * tile_size,
+      y * tile_size,
+      tile_size,
+      tile_size,
+    );
+
+    const svg_tile = filterSVG(svg, (element) => {
+      if (element instanceof SVGGraphicsElement) {
+        // Start with unprecise but fast `getBBox` method
+        let element_bbox = element.getBBox();
+
+        if (!intersects(bbox, element_bbox)) {
+          return false;
+        } else if (checkSize) {
+          // Compute bbox with more precise but expensive `getBoundingClientRect` method for width and height checking
+          element_bbox = element.getBoundingClientRect();
+          if (element_bbox.height < min_size && element_bbox.width < min_size) {
+            return false;
+          }
         }
-        y = 0;
       }
-    }
+
+      return true;
+    });
+
+    // Update viewport
+    svg_tile.setAttribute(
+      "viewBox",
+      `${bbox.x} ${bbox.y} ${bbox.width} ${bbox.height}`,
+    );
+
+    return svg_tile;
   }
 
-  // Stats
-  statsStore.currentZoom.set(z);
-  statsStore.tileCount.set(tiles.size);
-  statsStore.progress.set(
-    (100 * tiles.size) / statsStore.finalTilesCount.get(),
-  );
+  private createTilesChunked(
+    svg: SVGSVGElement,
+    max_zoom: number,
+    resolve: (value: TileStorage) => void,
+    x: number,
+    y: number,
+    z: number,
+  ) {
+    const initial_width = Math.ceil(
+      svg.width.baseVal.value / this.params.tileSize,
+    );
+    const initial_height = Math.ceil(
+      svg.height.baseVal.value / this.params.tileSize,
+    );
 
-  requestAnimationFrame(() =>
-    createTilesChunked(svg, max_zoom, tiles, resolve, x, y, z),
-  );
-}
+    for (let i = 0; i < 20; i++) {
+      // Process 20 tiles at a time
+      let svg_tile = this.createTile(svg, z, x, y, max_zoom);
+      let key = `${z}/${x}/${y}.svg`;
+      this.storage.set(key, svg_tile);
 
-export function createTiles(
-  svg: SVGSVGElement,
-  max_zoom: number,
-): Promise<Map<string, SVGSVGElement>> {
-  const tiles = new Map<string, SVGSVGElement>();
+      x++;
+      if (x >= initial_width * Math.pow(2, z)) {
+        x = 0;
+        y++;
+        if (y >= initial_height * Math.pow(2, z)) {
+          z++;
+          if (z > max_zoom) {
+            resolve(this.storage);
+            return;
+          }
+          y = 0;
+        }
+      }
+    }
 
-  return new Promise<Map<string, SVGSVGElement>>((resolve) => {
-    createTilesChunked(svg, max_zoom, tiles, resolve, 0, 0, 0);
-  });
+    // Stats
+    statsStore.currentZoom.set(z);
+    statsStore.tileCount.set(this.storage.size);
+    statsStore.progress.set(
+      (100 * this.storage.size) / statsStore.finalTilesCount.get(),
+    );
+
+    requestAnimationFrame(() =>
+      this.createTilesChunked(svg, max_zoom, resolve, x, y, z),
+    );
+  }
+
+  createTiles(svg: SVGSVGElement, max_zoom: number): Promise<TileStorage> {
+    return new Promise<TileStorage>((resolve) => {
+      this.createTilesChunked(svg, max_zoom, resolve, 0, 0, 0);
+    });
+  }
 }
