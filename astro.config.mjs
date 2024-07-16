4e12b1e09312b28283dc0cb74d66e939452b72fd
GaspardCulis <gasdev.lp@gmail.com>
2024-07-16 14:25:35 +0200

    Revert "feat: etup GH pages deployment GH action"
    This reverts commit e86dc67ba9efba9813a514a27eba049f2223ae58.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://GaspardCulis.github.io",
-  base: "/svguez",
   integrations: [tailwind()],
 });
